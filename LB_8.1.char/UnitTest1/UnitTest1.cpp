2386f58e11a697f85a128a588ff7975a5628dc8f
tarashrynchukRI <145685626+tarashrynchukRI@users.noreply.github.com>
2023-11-07 15:23:18 +0200

    Add files via upload

---
+++ LB_8.1.char/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB_8.1.char/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[] = "abcabcabc";
+			int res;
+			res = Count(str);
+			Assert::AreEqual(res, 3);
+		}
+	};
+}
